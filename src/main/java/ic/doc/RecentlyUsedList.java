9fbe4d047a54c7d24ea45dc79e4332c66c98f03d
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-10-08 18:22:02 +0100

    Refactored code

--- src/main/java/ic/doc/RecentlyUsedList.java
+++ src/main/java/ic/doc/RecentlyUsedList.java
@@ -1,21 +1,12 @@
 package ic.doc;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class RecentlyUsedList<T> {
-  protected List<T> ls;
-
-  RecentlyUsedList() {
-    this.ls = new ArrayList<>(0);
-  }
-
-  public void add(T x) {
-    ls.remove(x);
-    ls.add(0, x);
-  }
-
-  public T at(int x) {
-    return ls.get(x);
+public class RecentlyUsedList<T> extends ArrayList<T> {
+  @Override
+  public boolean add(T x) {
+    this.remove(x);
+    this.add(0, x);
+    return true;
   }
 }
--- src/test/java/ic/doc/RecentlyUsedListTest.java
+++ src/test/java/ic/doc/RecentlyUsedListTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.fail;
 public class RecentlyUsedListTest {
   @Test
   public void empty_when_initialised() {
-    assertTrue(new RecentlyUsedList<Integer>().ls.isEmpty());
+    assertTrue(new RecentlyUsedList<Integer>().isEmpty());
   }
 
   @Test
@@ -18,16 +18,16 @@ public class RecentlyUsedListTest {
     list.add(3);
     list.add(5);
     list.add(2);
-    assertTrue(list.ls.contains(3));
-    assertTrue(list.ls.contains(5));
-    assertTrue(list.ls.contains(2));
+    assertTrue(list.contains(3));
+    assertTrue(list.contains(5));
+    assertTrue(list.contains(2));
   }
 
   @Test
   public void retrieve_items() {
     RecentlyUsedList<Integer> list = new RecentlyUsedList<>();
     list.add(3);
-    assertEquals(3, (int) list.at(0));
+    assertEquals(3, (int) list.get(0));
   }
 
   @Test
@@ -36,9 +36,9 @@ public class RecentlyUsedListTest {
     list.add(3);
     list.add(5);
     list.add(2);
-    assertEquals(2, (int) list.at(0));
-    assertEquals(5, (int) list.at(1));
-    assertEquals(3, (int) list.at(2));
+    assertEquals(2, (int) list.get(0));
+    assertEquals(5, (int) list.get(1));
+    assertEquals(3, (int) list.get(2));
   }
 
   @Test
@@ -49,7 +49,7 @@ public class RecentlyUsedListTest {
     list.add(2);
     list.add(5);
     int count = 0;
-    for (int e : list.ls) {
+    for (int e : list) {
       if (e == 5) {
         count++;
       }
@@ -57,6 +57,6 @@ public class RecentlyUsedListTest {
     if (count > 1) {
       fail("Contains duplicate elements");
     }
-    assertEquals(5, (int) list.at(0));
+    assertEquals(5, (int) list.get(0));
   }
 }
